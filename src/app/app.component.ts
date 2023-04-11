3a02cbf1779e8b23bef7e422051ddf40bd5374ac
Vlad Gorozhankin <Gorozhankin@bsu.edu.ru>
2023-04-11 11:05:41 +0300

    add validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-task-st';
+
+  public mask = ['+','7',' ','(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/,'-', /\d/, /\d/]
+  myForm: FormGroup = new FormGroup({
+
+    name: new FormControl('', Validators.required),
+    email: new FormControl('',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    phone_number: new FormControl(''),
+    theme: new FormControl('',Validators.required),
+    message: new FormControl('',Validators.required),
+    captcha: new FormControl('',Validators.required),
+  });
+
+  submitForm() {
+    console.log(this.myForm);
+  }
 }
